13802160c501624b5414d8bc45778b84f098172e
Siva <108004062+Siva18jr@users.noreply.github.com>
2022-08-26 15:28:42 +0530

    //update  coupon price

--- final.php
+++ final.php
@@ -5,6 +5,5 @@ require_once 'connection.php';
 $total = $_REQUEST['Total'];
 
 $query = mysqli_query($conn, "UPDATE cart SET total_price = '$total'") or die(mysqli_error());
-header('location:user_final_pay.php');
 
 ?>
