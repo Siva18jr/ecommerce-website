9f4c09be18c2cb39ea70e155bcbcf3c8f3608945
Siva <108004062+Siva18jr@users.noreply.github.com>
2022-08-26 15:05:03 +0530

    coupon code value get and discount

--- js/user.js
+++ js/user.js
@@ -30,3 +30,59 @@ window.onscroll = () =>{
    
    }
 }
+
+//enter coupon on input click
+
+function Coupon(){
+    
+    var coupon = $('#coupon').val();
+    var price = $('#price').val();
+    
+    if(coupon == ""){
+        
+        alert("Please enter a coupon code!");
+    
+    }else{
+        
+        $.post('user_discount.php', {coupon: coupon, price: price}, function(data){
+            
+            if(data == "error"){
+                
+                // alert("Invalid Coupon Code!");
+                $('#total').val(price);
+                $('#result').html('');
+            
+            }else{
+                
+                var json = JSON.parse(data);
+                $('#result').html("<h4 class='pull-right text-danger'>"+json.discount+"% Off</h4>");
+                $('#total').val(json.price);
+            
+            }
+        });
+    }
+}
+
+// get value from input and post it in a output
+
+function Total(){
+    
+    var final = $("#total").val();
+    //alert(final);
+    
+    $.post('final.php', {Total: final}, function(data){
+
+        if(data == "error"){
+
+            alert("unable to connect");
+
+        }
+
+        else{
+
+            // alert("perfect");
+            window.location.href = "user_final_pay.php";
+
+        }
+    });
+}
