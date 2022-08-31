ed450825f36a1933ee6488269a67036e07f348b4
Siva <108004062+Siva18jr@users.noreply.github.com>
2022-08-31 16:40:01 +0530

    title changed

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include 'add_to_cart.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data</title>
+    <title>Home</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="user.css">
     <link rel="stylesheet" href="admin_dashboard.css">
